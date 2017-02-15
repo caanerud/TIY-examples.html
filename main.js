5a21ce21ed7468da97b3d79848a9ed665c5f1e13
caanerud <chrisaanerud@Chriss-MacBook-Pro.local>
2017-02-15 11:24:04 -0500

    assignment2
    assignment2

---
+++ main.js
@@ -0,0 +1,82 @@
+console.log("hi");
+
+var jordon = 25;
+console.log(jordon);
+
+var dog = "dog";
+dog = "cat";
+console.log("dog");
+
+var number = 2;
+console.log(number);
+var sum = number + number + 1;
+console.log(sum);
+
+var firstNumber = 50;
+var sum = firstNumber + firstNumber;
+console.log(sum);
+
+var vase = ("Vase");
+console.log(vase);
+
+var age = 50;
+var age = 15
+if (age < 12) {
+  console.log("You're a kid.")
+
+} else if (age < 19){
+  console.log("You're a teenager.")
+} else {
+  console.log("You're probably an adult")
+}
+
+var givenName;
+var greeting;
+console.log(givenName);
+
+greeting = ("Hello, how are you ");
+givenName = "Brett";
+console.log(givenName);
+givenName = givenName;
+console.log(givenName);
+console.log("Hello, how are you Brett?");
+console.log(greeting + givenName + "?");
+
+var high = 50;
+var low = 10;
+var math = high - low;
+math = high - "5";
+console.log(math);
+
+
+var name;
+var birthYear;
+var currentYear;
+
+name = "Chris";
+birthYear = 1979;
+currentYear = 2017;
+
+console.log(name + " is " + (currentYear - birthYear) + " years old");
+
+var age = 100;
+
+if (age > 100) {
+  console.log("You're over 100.")
+
+} else if (age < 100){
+  console.log("You're under 100.")
+} else if (age = 100){
+  console.log("You're 100")
+}
+
+var myAge = 75
+var isold;
+if (myAge > 100){
+  isold = true;
+} else if (myAge < 100){
+  isold = false;
+} else {
+
+}
+console.log(isold);
